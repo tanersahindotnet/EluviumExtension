c4346c175c89464bc70c588d3642bc3b9d28c732
tanersahincom <ts@tanersahin.com>
2020-10-01 15:19:38 +0300

    Design improvements

--- angular/src/app/privacy/privacy.component.ts
+++ angular/src/app/privacy/privacy.component.ts
@@ -9,7 +9,11 @@ import { BottomNavItem } from 'ngx-bottom-nav';
 })
 export class PrivacyComponent {
   items: BottomNavItem[];
-  constructor(private translate: TranslateService) { this.setupNavigation(); }
+  constructor(private translate: TranslateService)
+  {
+    this.setupNavigation();
+    this.checkSettings();
+  }
   blockAds=true;
   webRtc=true;
   fingerprint=true;
@@ -17,6 +21,111 @@ export class PrivacyComponent {
   disableFlash=true;
   spoofingScreen=true;
   browserProtection=true;
+  blockadsOnChange(toggle)
+  {
+    if(toggle)
+    {
+      localStorage.removeItem('blockAds');
+    }
+    else
+    {
+      localStorage.setItem('blockAds','false');
+    }
+    this.checkAllSettings();
+  }
+  webRtcOnChange(toggle)
+  {
+    if(toggle)
+    {
+      localStorage.removeItem('webRtc');
+    }
+    else
+    {
+      localStorage.setItem('webRtc','false');
+    }
+    this.checkAllSettings();
+  }
+  fingerprintOnChange(toggle)
+  {
+    if(toggle)
+    {
+      localStorage.removeItem('fingerprint');
+    }
+    else
+    {
+      localStorage.setItem('fingerprint','false');
+    }
+    this.checkAllSettings();
+  }
+  clearCookiesOnChange(toggle)
+  {
+    if(toggle)
+    {
+      localStorage.removeItem('clearCookies');
+    }
+    else
+    {
+      localStorage.setItem('clearCookies','false');
+    }
+    this.checkAllSettings();
+  }
+  disableFlashOnChange(toggle)
+  {
+    if(toggle)
+    {
+      localStorage.removeItem('disableFlash');
+    }
+    else
+    {
+      localStorage.setItem('disableFlash','false');
+    }
+    this.checkAllSettings();
+  }
+  spoofingScreenOnChange(toggle)
+  {
+    if(toggle)
+    {
+      localStorage.removeItem('spoofingScreen');
+    }
+    else
+    {
+      localStorage.setItem('spoofingScreen','false');
+    }
+    this.checkAllSettings();
+  }
+  protectionOnChange(toggle)
+  {
+    if(toggle.checked)
+    {
+      this.blockAds=true;
+      this.webRtc=true;
+      this.fingerprint=true;
+      this.clearCookies=true;
+      this.disableFlash=true;
+      this.spoofingScreen=true;
+      localStorage.removeItem('blockAds')
+      localStorage.removeItem('webRtc')
+      localStorage.removeItem('fingerprint')
+      localStorage.removeItem('clearCookies')
+      localStorage.removeItem('disableFlash')
+      localStorage.removeItem('spoofingScreen')
+    }
+    else
+    {
+      this.blockAds=false;
+      this.webRtc=false;
+      this.fingerprint=false;
+      this.clearCookies=false;
+      this.disableFlash=false;
+      this.spoofingScreen=false;
+      localStorage.setItem('blockAds','false')
+      localStorage.setItem('webRtc','false')
+      localStorage.setItem('fingerprint','false')
+      localStorage.setItem('clearCookies','false')
+      localStorage.setItem('disableFlash','false')
+      localStorage.setItem('spoofingScreen','false')
+    }
+  }
   private setupNavigation() {
     let vault;
     let generator;
@@ -35,5 +144,39 @@ export class PrivacyComponent {
         {icon: 'settings', label: settings, routerLink: '/setting'},
         {icon: 'security', label: security, routerLink: '/privacy'}
       ];
+  }
+  private checkSettings()
+  {
+    if(localStorage.getItem('blockAds') != null)
+    {
+      this.blockAds = false;
+    }
+    if(localStorage.getItem('webRtc') != null)
+    {
+      this.webRtc = false;
+    }
+    if(localStorage.getItem('fingerprint') != null)
+    {
+      this.fingerprint = false;
+    }
+    if(localStorage.getItem('clearCookies') != null)
+    {
+      this.clearCookies = false;
+    }
+    if(localStorage.getItem('disableFlash') != null)
+    {
+      this.disableFlash = false;
+    }
+    if(localStorage.getItem('spoofingScreen') != null)
+    {
+      this.spoofingScreen = false;
+    }
+  }
+  private checkAllSettings()
+  {
+    if(!this.blockAds && !this.webRtc && !this.fingerprint && !this.clearCookies && !this.disableFlash && !this.spoofingScreen)
+    {
+      this.browserProtection=false;
+    }
   }
 }
