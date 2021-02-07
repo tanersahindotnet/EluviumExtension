21af4b01a3d95d8873f4bd1d6bae75f0a6b14e35
Taner Şahin <ts@tanersahin.com>
2021-02-07 18:22:19 +0300

    Prettier

--- chrome/src/backgroundPage.ts
+++ chrome/src/backgroundPage.ts
@@ -1,117 +1,117 @@
 chrome.windows.onRemoved.addListener(function (windowid) {
-  localStorage.removeItem("password");
-  localStorage.removeItem("tempPass");
-});
+  localStorage.removeItem('password')
+  localStorage.removeItem('tempPass')
+})
 
 chrome.runtime.onInstalled.addListener(function (details) {
-  localStorage.removeItem("password");
-  localStorage.removeItem("tempPass");
-});
+  localStorage.removeItem('password')
+  localStorage.removeItem('tempPass')
+})
 
 chrome.tabs.onRemoved.addListener(function (tabId) {
   chrome.windows.getAll({ populate: true }, function (windows) {
-    let i = 0;
+    let i = 0
     windows.forEach(function (window) {
       window.tabs.forEach(function (tab) {
-        i--;
-      });
-    });
+        i--
+      })
+    })
     if (i === 0) {
-      localStorage.removeItem("password");
-      localStorage.removeItem("tempPass");
-      if (localStorage.getItem("clearCookies") === null) {
-        erase();
+      localStorage.removeItem('password')
+      localStorage.removeItem('tempPass')
+      if (localStorage.getItem('clearCookies') === null) {
+        erase()
       }
     }
-  });
-});
+  })
+})
 
 chrome.tabs.onCreated.addListener(function (tab) {
   chrome.windows.getAll({ populate: true }, function (windows) {
-    let i = 0;
+    let i = 0
     windows.forEach(function (window) {
       window.tabs.forEach(function () {
-        i++;
-      });
-    });
+        i++
+      })
+    })
     if (i === 0 || i === 1) {
-      localStorage.removeItem("password");
-      localStorage.removeItem("tempPass");
+      localStorage.removeItem('password')
+      localStorage.removeItem('tempPass')
     }
-  });
-});
+  })
+})
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const tab = tabs[0];
-    setNotifyIcon(tab);
-  });
+    const tab = tabs[0]
+    setNotifyIcon(tab)
+  })
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    setNotifyIcon(tab);
-  });
-});
+    setNotifyIcon(tab)
+  })
+})
 
-function setNotifyIcon(tab) {
-  localStorage.setItem("activeUrl", extractHostname(tab.url));
-  const urlList: any[] = JSON.parse(localStorage.getItem("urlList"));
-  const activeUrlName = extractHostname(tab.url);
+function setNotifyIcon (tab) {
+  localStorage.setItem('activeUrl', extractHostname(tab.url))
+  const urlList: any[] = JSON.parse(localStorage.getItem('urlList'))
+  const activeUrlName = extractHostname(tab.url)
   for (let i = 0; i < urlList.length; i++) {
-    const matchingResult = isMatching(urlList[i].toLowerCase(), activeUrlName);
-    const matchingResult2 = activeUrlName.includes(urlList[i].toLowerCase());
+    const matchingResult = isMatching(urlList[i].toLowerCase(), activeUrlName)
+    const matchingResult2 = activeUrlName.includes(urlList[i].toLowerCase())
     if (matchingResult || matchingResult2) {
-      console.log(urlList[i].toLowerCase() + " similar to " + activeUrlName);
-      chrome.browserAction.setBadgeBackgroundColor({ color: "#175ddc" });
-      chrome.browserAction.setBadgeText({ text: "+1" });
-      break;
+      console.log(urlList[i].toLowerCase() + ' similar to ' + activeUrlName)
+      chrome.browserAction.setBadgeBackgroundColor({ color: '#175ddc' })
+      chrome.browserAction.setBadgeText({ text: '+1' })
+      break
     } else {
-      chrome.browserAction.setBadgeText({ text: "" });
+      chrome.browserAction.setBadgeText({ text: '' })
     }
   }
-  if (localStorage.getItem("spoofingScreen") === null) {
-    changeScreenResolution();
+  if (localStorage.getItem('spoofingScreen') === null) {
+    changeScreenResolution()
   }
 }
 
-let interval = localStorage.getItem("interval");
-if (interval !== "0" || interval !== null) {
+let interval = localStorage.getItem('interval')
+if (interval !== '0' || interval !== null) {
   check(8000, function () {
-    interval = localStorage.getItem("interval");
-    const expire = localStorage.getItem("expire");
-    const date = new Date();
-    const expiredDate = new Date(expire);
-    const result = Math.abs(expiredDate.getTime() - date.getTime());
+    interval = localStorage.getItem('interval')
+    const expire = localStorage.getItem('expire')
+    const date = new Date()
+    const expiredDate = new Date(expire)
+    const result = Math.abs(expiredDate.getTime() - date.getTime())
     if (result === 0 || result < 0) {
-      localStorage.removeItem("password");
-      localStorage.removeItem("tempPass");
+      localStorage.removeItem('password')
+      localStorage.removeItem('tempPass')
     }
-  });
+  })
 }
-const extractHostname = (url) => {
+const extractHostname = url => {
   try {
     // use URL constructor and return hostname
-    const hostName = new URL(url).hostname.replace("www.", "");
-    const removeFrom = hostName.lastIndexOf(".");
+    const hostName = new URL(url).hostname.replace('www.', '')
+    const removeFrom = hostName.lastIndexOf('.')
     const removedExtension = hostName.substring(
       0,
       hostName.length - (hostName.length - removeFrom)
-    );
-    return removedExtension;
+    )
+    return removedExtension
   } catch {
-    return " ";
+    return ' '
   }
-};
-
-function isMatching(a, b) {
-  return new RegExp("\\b(" + a.match(/\w+/g).join("|") + ")\\b", "gi").test(b);
 }
-function check(timeout, cb) {
-  let c = 0;
+
+function isMatching (a, b) {
+  return new RegExp('\\b(' + a.match(/\w+/g).join('|') + ')\\b', 'gi').test(b)
+}
+function check (timeout, cb) {
+  let c = 0
   setInterval(function () {
     if (++c >= timeout) {
-      c = 0;
-      setTimeout(cb, 0);
+      c = 0
+      setTimeout(cb, 0)
     }
-  });
+  })
 }
 //--------------------------------------------------Security Options --------------------------------------------------
 
@@ -125,87 +125,87 @@ Spoofing screen resolution Protection
 */
 
 // Remove WebRTC local and public IP leakage
-if (localStorage.getItem("webRtc") === null) {
+if (localStorage.getItem('webRtc') === null) {
   chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
     value: false,
-    scope: "regular",
-  });
+    scope: 'regular'
+  })
 
   chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
-    value: false,
-  });
+    value: false
+  })
 
   chrome.storage.local.set({
-    rtcIPHandling: "default_public_interface_only",
-  });
+    rtcIPHandling: 'default_public_interface_only'
+  })
 }
 
 // Change User agent Fingerprint protection
-if (localStorage.getItem("fingerprint") === null) {
+if (localStorage.getItem('fingerprint') === null) {
   var item = [
-    "Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0",
-  ];
-  var userAgent = item[Math.floor(Math.random() * item.length)];
+    'Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0'
+  ]
+  var userAgent = item[Math.floor(Math.random() * item.length)]
   var requestFilter = {
-    urls: ["<all_urls>"],
-  };
+    urls: ['<all_urls>']
+  }
   chrome.webRequest.onBeforeSendHeaders.addListener(
     function (details) {
-      details.requestHeaders.push({ name: "DNT", value: "1" });
-      var headers = details.requestHeaders;
+      details.requestHeaders.push({ name: 'DNT', value: '1' })
+      var headers = details.requestHeaders
       for (var i = 0, l = headers.length; i < l; ++i) {
-        if (headers[i].name == "User-Agent") {
-          break;
+        if (headers[i].name == 'User-Agent') {
+          break
         }
       }
       if (i < headers.length) {
-        headers[i].value = userAgent;
+        headers[i].value = userAgent
       }
-      return { requestHeaders: headers };
+      return { requestHeaders: headers }
     },
     requestFilter,
-    ["requestHeaders", "blocking"]
-  );
+    ['requestHeaders', 'blocking']
+  )
 }
 
 //Block Youtube Ads and Trackers
-if (localStorage.getItem("blockAds") === null) {
-  var numBlocked = 0;
+if (localStorage.getItem('blockAds') === null) {
+  var numBlocked = 0
   chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
       // It is easier to ignore requests instead of modifying them - everything seems to work fine
       // Note that get_midroll_info response contains playerAds describing all of the ads in the video
       const ignore =
-        details.url.indexOf("/get_video_info") != -1 ||
-        details.url.indexOf("/api/stats/ads") != -1 ||
-        details.url.indexOf("/pagead/conversion") != -1 ||
-        details.url.indexOf("ad_companion") != -1 ||
-        details.url.indexOf("get_midroll_info") != -1;
+        details.url.indexOf('/get_video_info') != -1 ||
+        details.url.indexOf('/api/stats/ads') != -1 ||
+        details.url.indexOf('/pagead/conversion') != -1 ||
+        details.url.indexOf('ad_companion') != -1 ||
+        details.url.indexOf('get_midroll_info') != -1
       if (ignore) {
-        numBlocked++; // keep track of blocked requests
+        numBlocked++ // keep track of blocked requests
         //chrome.browserAction.setBadgeText({text: ''});
         //chrome.browserAction.setBadgeBackgroundColor({color: "#FF0000"});
         //hrome.browserAction.setBadgeText({text: (numBlocked > 99) ? "99+" : `${numBlocked}`});
       }
-      return { cancel: ignore };
+      return { cancel: ignore }
     },
-    { urls: ["<all_urls>"] },
-    ["blocking"]
-  );
+    { urls: ['<all_urls>'] },
+    ['blocking']
+  )
 }
 
 // Block all cookies except session cookies
-if (localStorage.getItem("blockCookies") === null) {
+if (localStorage.getItem('blockCookies') === null) {
   chrome.runtime.onInstalled.addListener(function () {
     chrome.contentSettings.cookies.set({
-      primaryPattern: "<all_urls>",
-      setting: "session_only",
-    });
-  });
+      primaryPattern: '<all_urls>',
+      setting: 'session_only'
+    })
+  })
 }
 
 // Remove all the browsing data
-function erase() {
+function erase () {
   chrome.browsingData.remove(
     {},
     {
@@ -220,46 +220,46 @@ function erase() {
       serverBoundCertificates: true,
       pluginData: true,
       passwords: true,
-      webSQL: true,
+      webSQL: true
     }
-  );
-  console.log("Erased");
+  )
+  console.log('Erased')
 }
 
 // Change the screen resolution for spoofing screen resolution attacks
-function changeScreenResolution() {
-  var height = Math.floor(Math.random() * 900) + 700;
-  var width = Math.floor(Math.random() * 800) + 700;
-  var availHeight = Math.floor(Math.random() * 800) + 600;
+function changeScreenResolution () {
+  var height = Math.floor(Math.random() * 900) + 700
+  var width = Math.floor(Math.random() * 800) + 700
+  var availHeight = Math.floor(Math.random() * 800) + 600
   screen = new (function () {
-    this.width = width;
-    this.height = height;
-    this.colorDepth = 24;
-    this.availHeight = availHeight;
-  })();
+    this.width = width
+    this.height = height
+    this.colorDepth = 24
+    this.availHeight = availHeight
+  })()
 }
-
 
 // ---------------------------------------------- Autofill-------------------------------------------------------------
 
-function fill(username, password) {
-  
-  chrome.tabs.executeScript(null,{
-    code: 'var username = "'+ username +'"; var password = "'+ password +'";'
-}, function() {
-    chrome.tabs.executeScript(null, {file: 'run.js'});
-});
-  
+function fill (username, password) {
+  chrome.tabs.executeScript(
+    null,
+    {
+      code:
+        'var username = "' + username + '"; var password = "' + password + '";'
+    },
+    function () {
+      chrome.tabs.executeScript(null, { file: 'run.js' })
+    }
+  )
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, _sender, sendResponse){
-      if(request.item) 
-      {
-        const userName = request.item.userName;
-        const password = request.item.sitePassword;
-        fill(userName, password);
-        sendResponse({done: true});
-      }
+chrome.runtime.onMessage.addListener(function (request, _sender, sendResponse) {
+  if (request.item) {
+    const userName = request.item.userName
+    const password = request.item.sitePassword
+    fill(userName, password)
+    sendResponse({ done: true })
   }
-);
+  return true
+})
--- chrome/src/contentPage.ts
+++ chrome/src/contentPage.ts
@@ -1,16 +1,18 @@
-window.opener = null;
+window.opener = null
 // Webapp content that is the youtube app #content(style-scope ytd-app)
-const content = document.getElementsByTagName("ytd-app")[0];
+const content = document.getElementsByTagName('ytd-app')[0]
 
-const callback = function(mutationsList, observer) {
+const callback = function (mutationsList, observer) {
   // click the skip button
-  const buttons:any = content.getElementsByClassName("ytp-ad-skip-button ytp-button");
-  buttons != null && buttons.length > 0 && buttons[0].click();
+  const buttons: any = content.getElementsByClassName(
+    'ytp-ad-skip-button ytp-button'
+  )
+  buttons != null && buttons.length > 0 && buttons[0].click()
   // remove any ad banners on the right
-  console.log("Banner Removed");
-  const banner = document.getElementById("companion");
-  if (banner != null) banner.style.display = "none";
-};
+  console.log('Banner Removed')
+  const banner = document.getElementById('companion')
+  if (banner != null) banner.style.display = 'none'
+}
 
-const observer = new MutationObserver(callback);
-observer.observe(content, {childList: true, subtree: true});
+const observer = new MutationObserver(callback)
+observer.observe(content, { childList: true, subtree: true })
