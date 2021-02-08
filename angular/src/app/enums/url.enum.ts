7b80000373f9f142bb61740ea2dd8e9d6b50a15c
Taner Şahin <ts@tanersahin.com>
2021-02-08 11:17:29 +0300

    Add enum file

---
+++ angular/src/app/enums/url.enum.ts
@@ -0,0 +1,12 @@
+export enum UrlEnum {
+  DASHBOARD = 'dashboard',
+  FIRSTLOGIN = 'firstLogin',
+  LOGIN = 'login',
+  GENERATOR = 'generator',
+  SETTING = 'setting',
+  ACCOUNT = 'account',
+  VERIFY = 'verify',
+  REGISTER = 'register',
+  PRIVACY = 'privacy',
+  WARNING = 'warning'
+}
